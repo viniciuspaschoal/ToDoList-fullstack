c407bbecf07353bcacb9ba21f315a1b53f923f36
viniciuspaschoal <viniciuspaschoal1847@gmail.com>
2025-06-24 22:34:18 -0300

    Configure Project

---
+++ src/test/java/com/vinicius/ToDoList_fullstack/ToDoListFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.ToDoList_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
